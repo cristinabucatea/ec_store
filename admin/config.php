b5ff19a1c63b54e3fb5cfc60d476d82a8dcf2b58
cristinabucatea <cristina.bucatea@gmail.com>
2016-02-04 00:18:33 +0200

    a start

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/ec_store/admin/');
+define('HTTP_CATALOG', 'http://localhost/ec_store/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/ec_store/admin/');
+define('HTTPS_CATALOG', 'http://localhost/ec_store/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp/www/ec_store/admin/');
+define('DIR_SYSTEM', 'C:/wamp/www/ec_store/system/');
+define('DIR_LANGUAGE', 'C:/wamp/www/ec_store/admin/language/');
+define('DIR_TEMPLATE', 'C:/wamp/www/ec_store/admin/view/template/');
+define('DIR_CONFIG', 'C:/wamp/www/ec_store/system/config/');
+define('DIR_IMAGE', 'C:/wamp/www/ec_store/image/');
+define('DIR_CACHE', 'C:/wamp/www/ec_store/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp/www/ec_store/system/storage/download/');
+define('DIR_LOGS', 'C:/wamp/www/ec_store/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/wamp/www/ec_store/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/wamp/www/ec_store/system/storage/upload/');
+define('DIR_CATALOG', 'C:/wamp/www/ec_store/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'online_store');
+define('DB_PASSWORD', '1234');
+define('DB_DATABASE', 'online_store');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
